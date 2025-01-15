94754cbddf3fa556318991cb7908c80f023ae3ee
Vlad <valdispatriarh@gmail.com>
2025-01-15 12:40:30 +0100

    test

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,10 +21,15 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  let result = false;
+  if (number >= 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 }
-
 /**
  * Returns the maximum of three numbers without using Array and Math classes methods.
  *
